5d1ae7afbc6aa134f2e01930cba690b51a694a6a
kochjosie <107821861+kochjosie@users.noreply.github.com>
2025-12-08 14:07:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-<button onclick="myFunction()">Click me</button>
+function getRandomIntInclusive(min,max) {
+	return Math.random();
+}
+
+<button onclick="getRandomIntInclusive(0,1)">Click me</button>
